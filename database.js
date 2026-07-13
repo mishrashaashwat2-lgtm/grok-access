73998311494bf2e633e8746c820b99896722b99f
mishrashaashwat2-lgtm <mishrashaashwat2@gmail.com>
2026-07-13 11:26:00 +0530

    Create database.js

---
+++ database.js
@@ -0,0 +1,80 @@
+const users = {
+
+    "GA-0000": {
+        password: "OWNER2026",
+        name: "Shaashwat Misra",
+        role: "ADMINISTRATOR",
+        clearance: "LEVEL 5",
+        status: "ACTIVE",
+        gender: "Male",
+        serial: "GA-SN-000001",
+        theme: "gold"
+    },
+
+    "GA-0003": {
+        password: "PM2026",
+        name: "Pranav Maurya",
+        role: "CO-ADMINISTRATOR",
+        clearance: "LEVEL 4",
+        status: "ACTIVE",
+        gender: "Male",
+        serial: "GA-SN-000003",
+        theme: "blue"
+    },
+
+    "GA-0007": {
+        password: "NM2026",
+        name: "Namita Mishra",
+        role: "MEMBER",
+        clearance: "LEVEL 1",
+        status: "ACTIVE",
+        gender: "Female",
+        serial: "GA-SN-000007",
+        theme: "green"
+    },
+
+    "GA-0002": {
+        password: "MN2026",
+        name: "Mann",
+        role: "MEMBER",
+        clearance: "LEVEL 1",
+        status: "ACTIVE",
+        gender: "Male",
+        serial: "GA-SN-000002",
+        theme: "green"
+    },
+
+"GA-0004": {
+    password: "SH2026",
+    name: "Shubh Mishra",
+    role: "UI DEVELOPER",
+    clearance: "LEVEL 3",
+    status: "ACTIVE",
+    gender: "Male",
+    serial: "GA-SN-000004",
+    theme: "purple"
+},
+
+    "GA-0005": {
+        password: "AS2026",
+        name: "Aditya Sharma",
+        role: "MEMBER",
+        clearance: "LEVEL 1",
+        status: "ACTIVE",
+        gender: "Male",
+        serial: "GA-SN-000005",
+        theme: "green"
+    },
+
+    "GA-0006": {
+        password: "AP2026",
+        name: "Anubhav Pushpak",
+        role: "MEMBER",
+        clearance: "LEVEL 1",
+        status: "ACTIVE",
+        gender: "Male",
+        serial: "GA-SN-000006",
+        theme: "green"
+    }
+
+};
