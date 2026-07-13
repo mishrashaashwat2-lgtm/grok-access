331cb15b515e48cec588798cb7d3ba7d8a7d3232
mishrashaashwat2-lgtm <mishrashaashwat2@gmail.com>
2026-07-13 11:24:57 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    portalName: "GROK ACCESS",
+    version: "v5.0",
+    organization: "Grok Access Security Division",
+
+    website: "https://mishrashaashwat2-lgtm.github.io/grok-access/",
+
+    issueDate: "12 July 2026",
+
+    support: "Administrator",
+
+    systemStatus: "ONLINE",
+
+    defaultClearance: "LEVEL 1",
+
+    adminClearance: "LEVEL 5",
+
+    coAdminClearance: "LEVEL 4"
+};
