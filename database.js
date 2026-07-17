8152bf05e2a2db2ac2de71251531906b6098c6f9
mishrashaashwat2-lgtm <mishrashaashwat2@gmail.com>
2026-07-17 20:39:03 +0530

    Update database.js

--- database.js
+++ database.js
@@ -2,6 +2,7 @@ const users = {
 
     "GA-0000": {
         password: "OWNER2026",
+        firstLogin: true,
         name: "Shaashwat Misra",
         role: "ADMINISTRATOR",
         clearance: "LEVEL 5",
@@ -13,6 +14,7 @@ const users = {
 
     "GA-0003": {
         password: "PM2026",
+        firstLogin: true,
         name: "Pranav Maurya",
         role: "CO-ADMINISTRATOR",
         clearance: "LEVEL 4",
@@ -24,6 +26,7 @@ const users = {
 
     "GA-0007": {
         password: "NM2026",
+        firstLogin: true,
         name: "Namita Mishra",
         role: "MEMBER",
         clearance: "LEVEL 1",
@@ -35,6 +38,7 @@ const users = {
 
     "GA-0002": {
         password: "MN2026",
+        firstLogin: true,
         name: "Mann",
         role: "MEMBER",
         clearance: "LEVEL 1",
@@ -44,19 +48,21 @@ const users = {
         theme: "green"
     },
 
-"GA-0004": {
-    password: "SH2026",
-    name: "Shubh Mishra",
-    role: "UI DEVELOPER",
-    clearance: "LEVEL 3",
-    status: "ACTIVE",
-    gender: "Male",
-    serial: "GA-SN-000004",
-    theme: "purple"
-},
+    "GA-0004": {
+        password: "SH2026",
+        firstLogin: true,
+        name: "Shubh Mishra",
+        role: "UI DEVELOPER",
+        clearance: "LEVEL 3",
+        status: "ACTIVE",
+        gender: "Male",
+        serial: "GA-SN-000004",
+        theme: "purple"
+    },
 
     "GA-0005": {
         password: "AS2026",
+        firstLogin: true,
         name: "Aditya Sharma",
         role: "MEMBER",
         clearance: "LEVEL 1",
@@ -68,6 +74,7 @@ const users = {
 
     "GA-0006": {
         password: "AP2026",
+        firstLogin: true,
         name: "Anubhav Pushpak",
         role: "MEMBER",
         clearance: "LEVEL 1",
